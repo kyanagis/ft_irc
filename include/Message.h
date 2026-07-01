91289f691dec3ac60e80d244e4b76f117e0e5d0c
kyanagis <kyanagis42@gmail.com>
2026-07-01 11:17:46 +0900

    add: Message.h

---
+++ include/Message.h
@@ -0,0 +1,28 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+#include <string>
+#include <vector>
+#include <cstddef>
+
+class Message
+{
+public:
+	static Message parse(const std::string& line);
+
+	bool                            empty() const;
+	const std::string&              prefix() const;
+	const std::string&              command() const;
+	const std::vector<std::string>& params() const;
+	const std::string&              param(std::size_t index) const;
+	std::size_t                     size() const;
+
+private:
+	Message();
+
+	std::string              _prefix;
+	std::string              _command;
+	std::vector<std::string> _params;
+};
+
+#endif
