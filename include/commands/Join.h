e13ef2412d7fbe57dbcb20b0e64d66c9eaf19d49
kyanagis <kyanagis42@gmail.com>
2026-07-01 11:17:47 +0900

    add: Join.h

---
+++ include/commands/Join.h
@@ -0,0 +1,13 @@
+#ifndef JOIN_COMMAND_H
+#define JOIN_COMMAND_H
+
+#include "../ACommand.h"
+
+class JoinCommand : public ACommand
+{
+public:
+	void execute(Server& server, Client& client, const Message& msg);
+	bool needsRegistration() const;
+};
+
+#endif
