399f2783de3b7120dcc43c158954faa8553365e3
kyanagis <kyanagis42@gmail.com>
2026-07-01 11:17:47 +0900

    add: Invite.h

---
+++ include/commands/Invite.h
@@ -0,0 +1,13 @@
+#ifndef INVITE_COMMAND_H
+#define INVITE_COMMAND_H
+
+#include "../ACommand.h"
+
+class InviteCommand : public ACommand
+{
+public:
+	void execute(Server& server, Client& client, const Message& msg);
+	bool needsRegistration() const;
+};
+
+#endif
