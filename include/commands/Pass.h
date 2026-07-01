c002b2cdfa02d45659a393c475586b72bc3d0f91
kyanagis <kyanagis42@gmail.com>
2026-07-01 11:17:47 +0900

    add: Pass.h

---
+++ include/commands/Pass.h
@@ -0,0 +1,13 @@
+#ifndef PASS_COMMAND_H
+#define PASS_COMMAND_H
+
+#include "../ACommand.h"
+
+class PassCommand : public ACommand
+{
+public:
+	void execute(Server& server, Client& client, const Message& msg);
+	bool needsRegistration() const;
+};
+
+#endif
