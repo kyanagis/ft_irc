c2cb8f3e89bbb7b4000b56ad3d3f1ed8cba1f69a
kyanagis <kyanagis42@gmail.com>
2026-07-01 11:17:47 +0900

    add: IrcException.h

---
+++ include/IrcException.h
@@ -0,0 +1,23 @@
+#ifndef IRC_EXCEPTION_H
+#define IRC_EXCEPTION_H
+
+#include <stdexcept>
+#include <string>
+
+class IrcException : public std::runtime_error
+{
+public:
+	IrcException(int code, const std::string& target, const std::string& detail);
+	~IrcException() throw();
+
+	int                code() const throw();
+	const std::string& target() const throw();
+	const std::string& detail() const throw();
+
+private:
+	int         _code;
+	std::string _target;
+	std::string _detail;
+};
+
+#endif
