fbe4efdb23782da53966a25930138b3959557c83
kyanagis <kyanagis42@gmail.com>
2026-07-01 11:17:47 +0900

    add: Notice.h

---
+++ include/commands/Notice.h
@@ -0,0 +1,13 @@
+#ifndef NOTICE_COMMAND_H
+#define NOTICE_COMMAND_H
+
+#include "../ACommand.h"
+
+class NoticeCommand : public ACommand
+{
+public:
+	void execute(Server& server, Client& client, const Message& msg);
+	bool needsRegistration() const;
+};
+
+#endif
