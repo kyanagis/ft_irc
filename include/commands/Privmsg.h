fc069d7d2c5e3a2a1f1ba5b90d1c37c1a1b229e3
kyanagis <kyanagis42@gmail.com>
2026-07-01 11:17:47 +0900

    add: Privmsg.h

---
+++ include/commands/Privmsg.h
@@ -0,0 +1,13 @@
+#ifndef PRIVMSG_COMMAND_H
+#define PRIVMSG_COMMAND_H
+
+#include "../ACommand.h"
+
+class PrivmsgCommand : public ACommand
+{
+public:
+	void execute(Server& server, Client& client, const Message& msg);
+	bool needsRegistration() const;
+};
+
+#endif
