ada78e83ab5f72640fd5908d345bc3e12c8b3b92
kyanagis <kyanagis42@gmail.com>
2026-07-01 11:17:47 +0900

    add: Cap.h

---
+++ include/commands/Cap.h
@@ -0,0 +1,13 @@
+#ifndef CAP_COMMAND_H
+#define CAP_COMMAND_H
+
+#include "../ACommand.h"
+
+class CapCommand : public ACommand
+{
+public:
+	void execute(Server& server, Client& client, const Message& msg);
+	bool needsRegistration() const;
+};
+
+#endif
