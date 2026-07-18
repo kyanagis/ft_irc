0dc7f5958a736c2418a5ea7b67fce926b850b239
Moto-ken <motoken01315@gmail.com>
2026-07-19 03:41:20 +0900

    correction errer case

--- src/commands/Invite.cpp
+++ src/commands/Invite.cpp
@@ -9,55 +9,35 @@
 
 void InviteCommand::execute(Server& server, Client& client,
                             const Message& msg) {
-    if (msg.size() < 2) {
-        server.sendLine(
-            client,
-            Reply::numeric(server.serverName(), Reply::ERR_NEEDMOREPARAMS,
-                           client.nick(), "INVITE :Not enough parameters"));
-        return;
+    if (msg.size() < 2 || msg.param(0).empty() || msg.param(1).empty()) {
+        throw IrcException(Reply::ERR_NEEDMOREPARAMS, client.nick() + " INVITE",
+                           ":Not enough parameters");
     }
 
     const std::string& targetNick = msg.param(0);
     const std::string& channelName = msg.param(1);
     Client* targetClient = server.findClientByNick(targetNick);
     if (targetClient == 0) {
-        server.sendLine(
-            client, Reply::numeric(server.serverName(), Reply::ERR_NOSUCHNICK,
-                                   client.nick(),
-                                   targetNick + " :No such nick/channel"));
-        return;
+        throw IrcException(Reply::ERR_NOSUCHNICK, client.nick(),
+                           targetNick + " :No such nick/channel");
     }
     Channel* channel = server.findChannel(channelName);
     if (channel == 0) {
-        server.sendLine(
-            client,
-            Reply::numeric(server.serverName(), Reply::ERR_NOSUCHCHANNEL,
-                           client.nick(), channelName + " :No such channel"));
-        return;
+        throw IrcException(Reply::ERR_NOSUCHCHANNEL, client.nick(),
+                           channelName + " :No such channel");
     }
     if (!channel->hasMember(client)) {
-        server.sendLine(
-            client,
-            Reply::numeric(server.serverName(), Reply::ERR_NOTONCHANNEL,
-                           client.nick(),
-                           channelName + " :You're not on that channel"));
-        return;
+        throw IrcException(Reply::ERR_NOTONCHANNEL, client.nick(),
+                           channelName + " :You're not on that channel");
     }
     if (channel->inviteOnly() && !channel->isOperator(client)) {
-        server.sendLine(
-            client,
-            Reply::numeric(server.serverName(), Reply::ERR_CHANOPRIVSNEEDED,
-                           client.nick(),
-                           channelName + " :You're not channel operator"));
-        return;
+        throw IrcException(Reply::ERR_CHANOPRIVSNEEDED, client.nick(),
+                           channelName + " :You're not channel operator");
     }
     if (channel->hasMember(*targetClient)) {
-        server.sendLine(
-            client,
-            Reply::numeric(
-                server.serverName(), Reply::ERR_USERONCHANNEL, client.nick(),
-                targetNick + " " + channelName + " :is already on channel"));
-        return;
+        throw IrcException(
+            Reply::ERR_USERONCHANNEL, client.nick(),
+            targetNick + " " + channelName + " :is already on channel");
     }
 
     channel->invite(*targetClient);
