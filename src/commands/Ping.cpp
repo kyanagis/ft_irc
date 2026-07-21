860968c02878950a7c9f103dec13aa3dbdff4a52
Kento Motohashi <kemotoha@c5r3s10.42tokyo.jp>
2026-07-21 16:42:54 +0900

    OK

--- src/commands/Ping.cpp
+++ src/commands/Ping.cpp
@@ -8,8 +8,8 @@
 
 void PingCommand::execute(Server& server, Client& client, const Message& msg) {
     std::string response = "PONG " + server.serverName();
-    if (msg.size() > 0 && !msg.param(0).empty()) {
-        response += " :" + msg.param(0);
+    if (msg.size() == 0 || msg.param(0).empty()) {
+        throw IrcException(Reply::ERR_NOORIGIN, client.nick(), "No origin specified");
     }
     server.sendLine(client, Reply::from(server.serverName(), response));
 }
